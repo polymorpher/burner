dc17431f56a42ec90a2949dad68d905f6a119495
polymorpher <aaron@qokka.ai>
2022-10-10 01:11:47 -0500

    add some mainnet hardcoded params

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -50,7 +50,11 @@ const hardhatUserconfig: HardhatUserConfig = {
     mainnet: {
       url: process.env.MAINNET_URL,
       accounts: { mnemonic: process.env.MNEMONIC },
-      chainId: 1666600000
+      chainId: 1666600000,
+      live: true,
+      gasPrice: 150e+9,
+      gasMultiplier: 2,
+      gas: 10e+6
     }
   },
   gasReporter: {
