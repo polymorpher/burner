bb29cbe8f989edb711c51315414029f163543db3
polymorpher <aaron@qokka.ai>
2022-10-09 22:14:54 -0500

    env configurable supported assets, chain parameters

--- client/config.js
+++ client/config.js
@@ -1,7 +1,27 @@
 const debug = process.env.DEBUG
 
-export default {
+const config = {
   debug,
-  burnerContract: process.env.BURNER_CONTRACT || (debug ? '' : ''),
+  burnerContract: process.env.BURNER_CONTRACT || (debug ? '0xA6d556Ad54F208c10c147D50c463e0C4e3aE4016' : ''),
   explorer: process.env.EXPLORER_URL || 'https://explorer.harmony.one/#/tx/{{txId}}',
+  supportedAssets: process.env.SUPPORTED_ASSETS
+    ? JSON.parse(process.env.SUPPORTED_ASSETS)
+    : [
+        '0xc8C3562FF714c110298E1772a7A12e12ecF02a92',
+      ],
+  chainParameters: process.env.CHAIN_PARAMETERS
+    ? JSON.parse(process.env.CHAIN_PARAMETERS)
+    : {
+        chainId: '0x63564C40', // A 0x-prefixed hexadecimal string
+        chainName: 'Harmony Mainnet Shard 0',
+        nativeCurrency: {
+          name: 'ONE',
+          symbol: 'ONE',
+          decimals: 18
+        },
+        rpcUrls: ['https://api.harmony.one'],
+        blockExplorerUrls: ['https://explorer.harmony.one/']
+      }
 }
+console.log(config)
+export default config
