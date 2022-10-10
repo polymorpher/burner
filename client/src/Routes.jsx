83de4d1675eeb8a2b6e5d14650d92f0c0ab70f74
polymorpher <aaron@qokka.ai>
2022-10-10 00:01:50 -0500

    debug route

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import Burn from './Burn'
+import Debug from './Debug'
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/' render={() => <Burn />} />
-        <Route path='/gss' render={() => <Burn />} />
+        <Route path='/burn' render={() => <Burn />} />
+        <Route path='/debug' render={() => <Debug />} />
         <Redirect to='/' />
       </Switch>
     </BrowserRouter>
